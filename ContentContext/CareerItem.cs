f23513d49f7f3a8c28fe6376e00da2744083a9a9
mayardes <mayardes.oliveira@globalsys.com.br>
2024-05-07 19:43:20 +0100

    feat: working with lists

--- ContentContext/CareerItem.cs
+++ ContentContext/CareerItem.cs
@@ -2,6 +2,14 @@ namespace Balta.ContentContext;
 
 public class CareerItem
 {
+    public CareerItem(int id, string title, string description)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+    }
+
+    public int Id { get; set; }
     public int Order { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ foreach (var item in course.Modules)
 #region ModelingCareer
 
 var career = new Career(string.Empty, string.Empty);
-career.CareerItems.Add(new CareerItem());
+career.CareerItems.Add(new CareerItem(0, string.Empty, string.Empty));
 Console.WriteLine($"Total: {career.Total}");
 #endregion
 
@@ -32,4 +32,39 @@ foreach (var article in articles)
     Console.WriteLine($"Title: {article.Title}");
     Console.WriteLine($"Url: {article.Url}");
 }
+#endregion
+
+#region InteractingWithLists
+Console.WriteLine("Working with lists:");
+var courseOOP = new Course("Course Objected-Oriented programming", "");
+var courseAspNet = new Course("Course AspNet MVC programming", "");
+var courseCSharp = new Course("Course CSharp programming", "");
+
+var courses = new List<Course>();
+courses.Add(courseOOP);
+courses.Add(courseAspNet);
+courses.Add(courseCSharp);
+
+var careers = new List<Career>();
+var specialistCareer = new Career("Specialist .Net", string.Empty);
+careers.Add(specialistCareer);
+
+var careerItem = new CareerItem(1, "Starting here", "description career item");
+
+specialistCareer.CareerItems.Add(careerItem);
+
+foreach (var car in careers)
+{
+    Console.WriteLine($"Id: {car.Id}");
+    Console.WriteLine($"Title: {car.Title}");
+    Console.WriteLine($"Url: {car.Url}");
+    
+    foreach (var item in car.CareerItems.OrderByDescending(x => x.Order))
+    {
+        Console.WriteLine($"Id: {item.Id}");
+        Console.WriteLine($"Title: {item.Title}");
+        Console.WriteLine($"Description: {item.Description}");
+    }
+}
+
 #endregion
